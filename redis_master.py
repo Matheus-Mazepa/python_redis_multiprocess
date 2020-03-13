c6b721a635ba7f7cb480e07c5d7619fdbe64480b
Mazepa <mfmazepa@hotmail.com>
2020-03-12 21:57:09 -0300

    Matriz no redis

--- redis_master.py
+++ redis_master.py
@@ -1,21 +1,37 @@
 from time import sleep
 from redis import Redis
 from rq import Queue
-from redis_modules import print_as_function
+import numpy
+from redis_modules import multiplica_linha_coluna, cria_matriz
 
 if __name__ == "__main__":
   print "Initializing redis master"
   redis_conn = Redis(host='127.0.0.1',port=6379)
   queue_jobs = Queue('my_queue', connection=redis_conn)
-  jobs = []
-  for i in range(10):
-    job = queue_jobs.enqueue(print_as_function, "realizando trabalho {0}".format(i))
-    jobs.append(job)
 
-  for job in jobs:
-    print "Trabalhos enfileirados {0}".format(len(queue_jobs))
-    while job.result is None:
-      print "O trabalho {0} ainda nao foi concluido".format(job.id)
-      sleep(2)
-      print "Resultado {0}".format(job.result)
-    print job.result
+  linhas, colunas = 2, 2
+
+  matrizA = cria_matriz(linhas, colunas)
+  matrizB = cria_matriz(linhas, colunas)
+
+  matrizA = numpy.array(matrizA)
+  matrizB = numpy.array(matrizB)
+
+  matrizC = numpy.zeros(shape=(linhas, colunas))
+
+  jobs = []
+  for i in range(len(matrizA)):
+    jobs_local = []
+    for j in range(len(matrizA[0])):
+      job = queue_jobs.enqueue(multiplica_linha_coluna, matrizA[i], matrizB[:, j])
+      jobs_local.append(job)
+    jobs.append(jobs_local)
+
+  for i in range(len(matrizA)):
+    for j in range(len(matrizA[0])):
+      job = jobs[i][j]
+      while job.result is None:
+        sleep(2)
+      matrizC[i][j] = job.result
+
+  print matrizC
--- redis_modules.py
+++ redis_modules.py
@@ -1,5 +1,22 @@
 from time import sleep
+import random
 
 def print_as_function(argument):
   sleep(2)
   return argument
+
+def cria_matriz(linhas, colunas):
+  A = []
+  for i in range(linhas):
+    linha = []
+    for j in range(colunas):
+      linha = linha + [random.randint(1, 10)]
+    A = A + [linha]
+  return A
+
+def multiplica_linha_coluna(matrizA, matrizB):
+  valor = 0
+
+  for k in range(len(matrizB)):
+    valor = valor + matrizA[k] * matrizB[k]
+  return valor
